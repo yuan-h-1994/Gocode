45558ecf9b9905de1d139f2846fe27aa4bf854b0
koki-gen <yuanhonghui1126@gmail.com>
2021-06-11 17:32:29 +0900

    Rangeで分割ダウンロード

--- downloader/download.go
+++ downloader/download.go
@@ -2,28 +2,92 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 )
 
+const size = 4 //分割の数
+
+var url = "https://www.robots.ox.ac.uk/~vgg/data/paintings/painting_dataset_2021.csv"
+var fileph = "./painting_dataset_2021.csv"
+var muex sync.Mutex
+var wg = sync.WaitGroup{}
+
 func main() {
-	//args := os.Args
-	//url, fileph := args[1], args[2]
-	url, fileph := "https://github.com/yuan-h-1994/Gocode.git", "./GOcode.zip"
-	if err := Download(url, fileph); err != nil {
+	fmt.Println("ダウンロードスタート...")
+	filelen := Getfilelen()
+	fmt.Println("ファイルサイズ：", calculatelen(filelen))
+	file, err := os.Create(fileph)
+	if err != nil {
 		panic(err)
 	}
-
+	part := filelen/size + 1 //startとendを指定する
+	wg.Add(size)
+	//downloadを繰り返す
+	for i := 0; i < size; i++ {
+		go Download(url, file, filelen, i, part)
+	}
+	defer file.Close()
+	wg.Wait()
+	fmt.Println("ダウンロード終了。")
 }
 
-func Download(url string, fileph string) error {
+//ダウンロード機能
+func Download(url string, file *os.File, filelen int, i int, part int) {
+	var start, end int
+	fmt.Println(strconv.Itoa(i) + "番目の部分をダウンロードスタート")
+	muex.Lock()
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+
+	if i != 0 { //rangeのstartとendを指定
+		start = i*part + 1
+	} else {
+		start = 0
+	}
+	if i+1 == size {
+		end = filelen
+	} else {
+		end = (i + 1) * part
+	}
+
+	str := strconv.FormatInt(int64(start), 10) + "-" + strconv.FormatInt(int64(end), 10)
+	fmt.Println("Range"+strconv.Itoa(i), "byte="+str)
+	req.Header.Set("Range", "bytes="+str)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("client.Do err:%v", err)
+	}
+
+	fmt.Println(strconv.Itoa(i)+"番目のダウンロードの長さ:", resp.Header.Get("Content-Length"))
+	op, err := file.Seek(int64(start), 0)
+	fmt.Println("今までダウンロードの長さ:", op)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("read file error: %v", err)
+	}
+	if len(body) == 0 {
+		fmt.Println("no file")
+	}
+
+	wg.Done()
+	len, err := file.Write(body)
+	fmt.Println(strconv.Itoa(i)+"番目のファイルを書き込み、長さ：", len)
+	resp.Body.Close()
+	muex.Unlock()
+}
+
+//ファイルの長さを取得
+func Getfilelen() (filelen int) {
 	resp, err := http.Get(string(url))
 	if err != nil {
-		return err
+		panic(err)
 	}
-	defer resp.Body.Close()
 
 	filelenStr, ext := resp.Header["Content-Length"]
 	if !ext || len(filelenStr) == 0 {
@@ -31,23 +95,13 @@ func Download(url string, fileph string) error {
 	}
 
 	filelen, fileerr := strconv.Atoi(filelenStr[0])
+	fmt.Println("ファイルの長さ：", filelen)
 	if fileerr != nil {
 		fmt.Println(fileerr)
 		filelen = 0
 		fmt.Println("ファイルサイズ未知")
 	}
-	fmt.Println("ファイルサイズ：", calculatelen(filelen))
-
-	//resp.Header.Set("Range",fmt.Sprintf("bytes=%d-%d", 0, t.file_size))
-
-	file, err := os.Create(fileph)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	return err
+	return filelen
 }
 
 //ファイルサイズを計算する
